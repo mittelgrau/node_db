825fdce3eafbbd3fbc5018f268da6770e1060d3f
mittelgrau <Djessa@live.de>
2019-09-29 23:12:33 +0200

    try image url

--- _data/about.js
+++ _data/about.js
@@ -12,7 +12,13 @@ module.exports = {
             fieldtype: 'text',
             fielddescription: 'change this to something better',
             fieldcontent: 'testing this'
-        }
+        },
+        {
+            fieldname: 'headerImage',
+            fieldtype: 'image',
+            fielddescription: 'beautiful flower',
+            fieldcontent: 'https://images.unsplash.com/photo-1541093113199-a2e9d84e903f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1235&q=80'
+        }  
     ]
 }
 
